ec27489f4ad10108c24b3187625f2285b14be910
Alex <san4ezz.8989@gmail.com>
2020-05-25 18:11:42 +0300

    начал проект

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+const config = require('./config');
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extenede: true }));
+
+const arr = ['heloww', 'worl', 'test'];
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    arr,
+  });
+});
+
+app.get('/create', function (req, res) {
+  res.render('create');
+});
+
+app.post('/create', function (req, res) {
+  arr.push(req.body.text);
+  res.redirect('/');
+});
+
+app.listen(config.PORT, function () {
+  console.log(`Example app listening on port ${config.PORT}!`);
+});
